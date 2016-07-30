9acacb569ae4a438cd5b62c493f00a430d563294
cdeborja <cdeborja@gmail.com>
2016-07-30 18:03:35 -0400

    Reorganize code

--- getRankings.js
+++ getRankings.js
@@ -1,8 +1,12 @@
 var request = require('request');
+var readFile = require('fs').readFileSync;
+var handlebars = require('handlebars');
 
-module.exports = function getRankings (res) {
-    request('http://rank.shoryuken.com/api/top?game=UMVC3&size=20&format=json', function (error, response, data) {
+module.exports = function getRankings (req, res) {
+  console.log(req.params.name);
+    request('http://rank.shoryuken.com/api/player/name/' + req.params.name, function (error, response, data) {
       if (!error && response.statusCode == 200) {
+        //send takes a string as an arg
         res.send(render(data));
       }
   });
@@ -10,7 +14,12 @@ module.exports = function getRankings (res) {
 
 function render (data) {
   var parsedData = JSON.parse(data);
-  return (
-    "<p>" + parsedData[0].name + "</p>"
-  );
+  var source = readFile("./index.html", "UTF8");
+  var template = handlebars.compile(source);
+
+  var info = {
+    'name': parsedData.name
+  };
+
+  return template(info);
 }
--- rankings.js
+++ rankings.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 var getRankings = require('./getRankings');
+var readFile = require('fs').readFileSync;
 
-app.get('/', function (req, res) {
-  var rankings = getRankings(res);
+app.get('/index.css', function (req, res) {
+  console.log("GOT to css");
+  res.send(readFile("./index.css", "UTF8"));
+});
 
+app.get('/index.js', function (req, res) {
+  console.log("GOT to js");
+  res.send(readFile("./index.js", "UTF8"));
+});
+
+app.get('/name/:name', function (req, res) {
+  var rankings = getRankings(req, res);
 });
 
 app.listen(3000, function () {

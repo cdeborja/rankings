c83cc76d4c31576d6ebcb169ece0972f715caf16
cdeborja <cdeborja@gmail.com>
2016-08-01 15:17:43 -0400

    CSS changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   console.log('DOM Fully Loaded');
 
   var input = document.getElementsByClassName("user")[0];
-  var nameList;
+  var nameList, timer;
 
   input.addEventListener("keyup", function (e) {
     if (this.value.length > 2) {
@@ -25,9 +25,13 @@ function searchName(query) {
   xhr.onload = function() {
     if (xhr.status === 200) {
       nameList = JSON.parse(this.response);
-      document.getElementsByClassName("autocomplete")[0].innerHTML = "";
+      document.getElementsByClassName("autocomplete")[0].innerHTML = "<li>Player Name (Real Name)</li>";
       for (var i = 0; i < nameList.length; i++) {
-        document.getElementsByClassName("autocomplete")[0].innerHTML += ("<li>" + nameList[i][0] + "</li>");
+        if (nameList[i][1] === null) {
+          nameList[i][1] = "N/A";
+        }
+        var player = ("<li>" + nameList[i][0] + " (" + nameList[i][1] + ") </li>");
+        document.getElementsByClassName("autocomplete")[0].innerHTML += player;
       }
     }
     else {
